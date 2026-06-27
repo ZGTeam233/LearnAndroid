3766e364603fc80ada0bcd573a034a83b16134ac
ZGTeam233 <wsnzg6@outlook.com>
2026-06-27 10:50:12 +0800

    First commit

--- WebAppDemo/app/build.gradle.kts
+++ WebAppDemo/app/build.gradle.kts
@@ -27,6 +27,11 @@ android {
             }
         }
     }
+
+    buildFeatures {
+        viewBinding = true
+    }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
@@ -40,6 +45,8 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.material)
     testImplementation(libs.junit)
+    implementation("com.google.android.material:material:1.12.0")
+    implementation("androidx.coordinatorlayout:coordinatorlayout:1.2.0")
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(libs.androidx.junit)
 }
